cb4a93a48f01254c807da6d6ad6540aa63223bad
optomat <OptomatJs@protonmail.com>
2021-06-20 22:29:37 +0200

    add browser bar

--- frontend/scripts/browser.js
+++ frontend/scripts/browser.js
@@ -1,5 +1,26 @@
-function toto () {
-    console.log(document.querySelector('h1').textContent)
-} 
+const browseBar = document.getElementById('browse_bar')
 
-toto()
+function pushTag(value) {
+  const tags = document.getElementById("tags");
+
+  const tag = document.createElement("div");
+  tag.classList.add("tag");
+  tag.innerHTML = value.trim();
+
+  tags.appendChild(tag);
+}
+
+document.getElementById("add_tag").addEventListener("click", (e) => {
+    const value = browseBar.value.trim()
+
+    if (value && value.length >0) {
+        pushTag(value)
+        console.log(value)
+    }
+});
+
+function onstart() {
+  console.log(document.querySelector("h1").textContent);
+}
+
+onstart();
--- source/public/js/app.js
+++ source/public/js/app.js
@@ -15,7 +15,7 @@
   \*************************************/
 /***/ (() => {
 
-eval("function toto () {\r\n    console.log('aa')\r\n} \r\n\r\ntoto()\n\n//# sourceURL=webpack://open-seeker/./frontend/scripts/browser.js?");
+eval("const browseBar = document.getElementById('browse_bar')\r\n\r\nfunction pushTag(value) {\r\n  const tags = document.getElementById(\"tags\");\r\n\r\n  const tag = document.createElement(\"div\");\r\n  tag.classList.add(\"tag\");\r\n  tag.innerHTML = value.trim();\r\n\r\n  tags.appendChild(tag);\r\n}\r\n\r\ndocument.getElementById(\"add_tag\").addEventListener(\"click\", (e) => {\r\n    const value = browseBar.value.trim()\r\n\r\n    if (value && value.length >0) {\r\n        pushTag(value)\r\n        console.log(value)\r\n    }\r\n});\r\n\r\nfunction onstart() {\r\n  console.log(document.querySelector(\"h1\").textContent);\r\n}\r\n\r\nonstart();\r\n\n\n//# sourceURL=webpack://open-seeker/./frontend/scripts/browser.js?");
 
 /***/ })
 
