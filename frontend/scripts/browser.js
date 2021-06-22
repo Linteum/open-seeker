31b38dc03997fa888055e05eb049168820edd1ba
optomat <OptomatJs@protonmail.com>
2021-06-22 17:15:27 +0200

    check if tags already pushed

--- frontend/scripts/browser.js
+++ frontend/scripts/browser.js
@@ -1,26 +1,39 @@
-const browseBar = document.getElementById('browse_bar')
+const browseBar = document.getElementById("browse_bar");
+
+function addDiv(className, parentDiv) {
+  const div = document.createElement("div");
+  div.classList.add(className);
+  parentDiv.appendChild(div)
+
+  return div
+}
+
+function existingTags(tags, value) {
+  if (Object.values(tags).find((tag) => tag.innerHTML.trim() == value.trim()))
+    return true;
+
+  return false;
+}
 
 function pushTag(value) {
   const tags = document.getElementById("tags");
 
-  const tag = document.createElement("div");
-  tag.classList.add("tag");
-  tag.innerHTML = value.trim();
+  const tagsList = document.querySelectorAll(".tag");
 
-  tags.appendChild(tag);
+  if (!existingTags(tagsList, value)) {
+    const tag = addDiv("tag", tags)
+    tag.innerHTML = value.trim();
+  }
 }
 
 document.getElementById("add_tag").addEventListener("click", (e) => {
-    const value = browseBar.value.trim()
-
-    if (value && value.length >0) {
-        pushTag(value)
-        console.log(value)
-    }
+  const value = browseBar.value.trim();
+  if (value && value.length > 0) {
+    pushTag(value);
+    console.log(value);
+  }
 });
 
-function onstart() {
-  console.log(document.querySelector("h1").textContent);
-}
+function onLoad() {}
 
-onstart();
+onLoad();
