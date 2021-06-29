b52f2bc4114d761bc2ba9e5b77d7bca67bbf461c
optomat <OptomatJs@protonmail.com>
2021-06-29 18:12:17 +0200

    get thumbnauils from items

--- frontend/scripts/browser.js
+++ frontend/scripts/browser.js
@@ -2,16 +2,16 @@ const { icons } = require("../components");
 const openSeekerEndPoint = "http://localhost:8000/api/tags";
 const browseBar = document.getElementById("browse_bar");
 
+
 async function getGenres(str) {
-  const queryString = encodeURIComponent(str.trim())
+  const queryString = encodeURIComponent(str.trim());
 
   const res = await fetch(`${openSeekerEndPoint}?genres=${queryString}`);
   // console.log(res)
   if (res.ok) {
     const data = await res.json();
-    if (data.genres) return data.genres
-    return []
-    
+    if (data.genres) return data.genres;
+    return [];
   }
   return false;
 }
@@ -62,24 +62,106 @@ function enterValue() {
 }
 
 function updateOptions(parentList, values) {
-  parentList.innerHTML = ""
+  parentList.innerHTML = "";
   for (let value of values) {
-    const opt = document.createElement('option')
-    opt.value = value
-    parentList.appendChild(opt)
+    const opt = document.createElement("option");
+    opt.value = value;
+    parentList.appendChild(opt);
   }
 }
 
+function formatSearchQuery(tags, andMode = true) {
+  const options = {
+    fields: "identifier",
+    mediaType: "audio",
+    rows: "128",
+    uri: "https://archive.org/services/search/v1/scrape",
+    cursor: "",
+  };
+  options.query = `mediatype:${options.mediaType}`;
+  for (let tag of tags) {
+    if (andMode) options.query += `+AND+subject:"${tag}"`;
+  }
+  const uri = encodeURI(
+    `${options.uri}?q=${options.query}&fields=${options.fields}&count=${options.rows}${options.cursor}`
+  );
+  return uri;
+}
+
+function getTagList() {
+  const tags = document.querySelectorAll(".tag");
+  return Object.values(tags).map((el) => {
+    return el.dataset.value;
+  });
+}
+
+async function getIdsFromAO(uri) {
+  const res = await fetch(uri)
+  if (res.ok) {
+    const data = await res.json()
+
+    return data.items
+  }
+  return false
+}
+
+async function getMetaFromId (id) {
+  const res = await fetch(`https://archive.org/metadata/${id}`)
+
+  if (res.ok) {
+    const data = await res.json()
+    return data
+    // console.log(data)
+  }
+  return false
+}
+
+function reformatAOItems() {
+
+} 
+
+function getThumbsFromAOItems (items) {
+  // console.log(items)
+  const thumbs = items.map(item => {
+    const files = item.files
+
+    const thumb = files.find(file => {
+      return file.format.includes("JPEG Thumb") || file.format.includes("Item Tile") 
+    })
+    // console.log(thumb)
+    if (thumb) return `${item.d1}${item.dir}/${thumb.name}`
+    return ''
+  })
+
+  return thumbs
+}
+async function research() {
+  const tags = getTagList();
+  const uri = formatSearchQuery(tags);
+  const names = await getIdsFromAO(uri)
+  const items = await Promise.all(names.map(item => {
+    return getMetaFromId(item.identifier)
+  }))
+
+  // console.log(items[0])
+  const thumbs = getThumbsFromAOItems(items)
+
+  // console.log(thumbs);
+}
+// ia601901.us.archive.org/32/items/001WeedProblem/001.mp3
 // addevent listeners
 
 browseBar.addEventListener("input", async (e) => {
   // console.log(e.target.value);
   const genres = await getGenres(e.target.value);
-  updateOptions(document.getElementById("genres"), genres)
-
+  updateOptions(document.getElementById("genres"), genres);
 });
 document.getElementById("add_tag").addEventListener("click", (e) => {
   enterValue();
+});
+
+document.getElementById("research").addEventListener("click", (e) => {
+  research();
 });
 
 browseBar.addEventListener("keydown", (e) => {
@@ -88,7 +170,7 @@ browseBar.addEventListener("keydown", (e) => {
       enterValue();
       break;
     case "Enter":
-      // launch research
+      research();
       break;
   }
 });
